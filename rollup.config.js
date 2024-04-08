0711c6aaf62323aa38cff3ce43f955502b536f71
scammi <scammi@gmail.com>
2024-04-07 23:54:28 -0300

    b

--- rollup.config.js
+++ rollup.config.js
@@ -6,17 +6,19 @@ export default {
   input: './dist/compiled/index.js', // Entry point of your TypeScript file
   output: {
     file: './dist/index.js', // Output file name
-    format: 'cjs', // Output format (can be 'cjs', 'es', 'umd', 'amd')
+    format: 'umd', // Output format (can be 'cjs', 'es', 'umd', 'amd')
     name: 'blockus-eth-payment', // Name of the global object if format is 'umd'
   },
   plugins: [
     typescript(), // Use rollup-plugin-typescript2 to handle TypeScript files
-    commonjs({
-      include: /node_modules/,
-      namedExports: {
-        'ethers': Object.keys(ethers),
-      }
-    }), 
     resolve(),
   ],
+  external: [
+    'bn.js',
+    'js-sha3',
+    'hash.js',
+    'aes-js',
+    'scrypt-js',
+    'bech32'
+  ]
 };
