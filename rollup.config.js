da2b5c1c38f73eb1f2f73ed96b4f77cf95a0ec30
scammi <scammi@gmail.com>
2024-04-07 23:37:03 -0300

    test

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,22 @@
 import typescript from 'rollup-plugin-typescript2';
-
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import { ethers } from 'ethers';
 export default {
   input: './dist/compiled/index.js', // Entry point of your TypeScript file
   output: {
     file: './dist/index.js', // Output file name
-    format: 'umd', // Output format (can be 'cjs', 'es', 'umd', 'amd')
+    format: 'cjs', // Output format (can be 'cjs', 'es', 'umd', 'amd')
     name: 'blockus-eth-payment', // Name of the global object if format is 'umd'
   },
   plugins: [
     typescript(), // Use rollup-plugin-typescript2 to handle TypeScript files
+    commonjs({
+      include: /node_modules/,
+      namedExports: {
+        'ethers': Object.keys(ethers),
+      }
+    }), 
+    resolve(),
   ],
 };
